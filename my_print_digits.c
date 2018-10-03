3b817f06e00031f2b95436a604815f6c297ead8e
Julien Ferrier <jferrier@localhost.localdomain>
2018-10-03 17:44:03 +0200

    day03

---
+++ my_print_digits.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2018
+** my_print_digits.c
+** File description:
+** task03
+*/
+
+
+int my_print_digit(void)
+{
+    int number_digit = 48;
+	
+    	while (number_digit < 58) {
+	    my_putchar(number_digit);
+	    number_digit = number_digit + 1;
+	}
+	return (0);
+}
+
+
+    
+
--- my_print_revalpha.c
+++ my_print_revalpha.c
@@ -1,16 +1,17 @@
 /*
 ** EPITECH PROJECT, 2018
-** my_print_alpha.c
+** my_print_revalpha.c
 ** File description:
-** task01
+** task02
 */
+
 
 int my_print_alpha(void)
 {
     int letter;
-    
-    letter = 97;
-    while (letter < 123) {
+
+    letter = 96;
+    while (letter < 122) {
 	my_putchar(letter);
 	letter = letter + 1;
     }
